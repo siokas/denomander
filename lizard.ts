914293583e3ebdec58faa77c5ba9d8f8b28dfada
Apostolos Siokas <apostolossiokas@gmail.com>
2020-05-20 11:29:49 +0300

    add readme for lizard

--- lizard.ts
+++ lizard.ts
@@ -1,10 +1,20 @@
-import { Lizard } from "./mod.ts";
+import { Lizard, program } from "./mod.ts";
+
+Lizard.appDetails({
+  app_name: "Lizy",
+  app_description: "A new Lizard App",
+  app_version: "1.0.0"
+});
 
 Lizard.command("clone [url]", clone)
   .option("-c, --color", "the color")
   .describe(
     "this is a description",
   );
+
+Lizard.command('serve', ()=>{
+  console.log(program.port);
+}).requiredOption('-p --port', 'ppp');
 
 function clone(url: string) {
   console.log("clone from ..." + url);
--- src/Lizard.ts
+++ src/Lizard.ts
@@ -1,5 +1,6 @@
 import { Kernel } from "./Kernel.ts";
 import { Command } from "./Command.ts";
+import { AppDetails } from "./types.ts";
 
 export class Lizard {
   public allcommands: any = [];
@@ -7,6 +8,32 @@ export class Lizard {
 
   constructor(app: Kernel) {
     this.app = app;
+  }
+
+  appDetails(app_details:AppDetails){
+    this.app.app_name = app_details.app_name;
+    this.app.app_description = app_details.app_description;
+    this.app.app_version = app_details.app_version;
+
+    return this;
+  }
+
+  appName(name:string){
+    this.app.app_name = name;
+
+    return this;
+  }
+
+  appDescription(description:string){
+    this.app.app_description = description;
+
+    return this;
+  }
+
+  appVersion(version:string){
+    this.app.app_version = version;
+
+    return this;
   }
 
   command(name: string, callback: Function) {
