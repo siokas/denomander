016427889afff5f6e85dd58fa2080151d884b9c7
Apostolos Siokas <apostolossiokas@gmail.com>
2020-05-20 22:07:29 +0300

    testing...

--- lizard_installer.sh
+++ lizard_installer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sSf "https://deno.land/x/denomander/.lizard_installer.ts"
-curl -sSf "https://deno.land/x/denomander/lizard.ts"
+curl -sSf "https://deno.land/x/denomander/.lizard_installer.ts" -o ./.lizard_installer.ts
+curl -sSf "https://deno.land/x/denomander/lizard.ts" -o ./lizard.ts
