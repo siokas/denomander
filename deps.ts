351ceb7383b38b4a9d60d4e49f1fd394986577b3
sera1mu <ifrea0281@gmail.com>
2021-02-28 18:45:26 +0900

    Bump to std@0.88.0

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
 export const test = Deno.test;
-export { parse } from "https://deno.land/std@0.86.0/flags/mod.ts";
+export { parse } from "https://deno.land/std@0.88.0/flags/mod.ts";
 export {
   blue,
   bold,
   green,
   red,
   yellow,
-} from "https://deno.land/std@0.86.0/fmt/colors.ts";
+} from "https://deno.land/std@0.88.0/fmt/colors.ts";
 export {
   assert,
   assertEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.86.0/testing/asserts.ts";
+} from "https://deno.land/std@0.88.0/testing/asserts.ts";
